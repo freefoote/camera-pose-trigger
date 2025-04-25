aa8386595b47a2b8f5949244e187579d297601d7
Daniel Foote <freefoote@gmail.com>
2025-04-25 03:44:59 +0000

    Very crude annotated preview on front end.

--- app.py
+++ app.py
@@ -5,3 +5,65 @@ load_dotenv()
 import logging
 import threading
 import time
+import os
+
+from pyee.base import EventEmitter
+
+from fastapi import FastAPI, WebSocket, Response
+from contextlib import asynccontextmanager
+from nicegui import app as ngapp, ui, run
+
+import cv2
+import numpy as np
+import base64
+
+import landmarker
+
+# Fire up the landmarker and event emitter.
+event_emitter = EventEmitter()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    print("Starting LM thread...")
+    lm_thread = threading.Thread(target=landmarker.thread_run_mediapipe, args=(event_emitter, os.getenv('CAMERA'), os.getenv('MODELFILE')))
+    lm_thread.start()
+    print("LM Thread running, yiedling back.")
+    yield
+    # Clean up the ML models and release the resources
+    print("Lifecycle complete. Closing thread.")
+    event_emitter.emit('kill_lm_thread')
+
+# Set up FastAPI
+app = FastAPI(lifespan=lifespan)
+
+# The encoded frame to send back to the client.
+black_1px = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjYGBg+A8AAQQBAHAgZQsAAAAASUVORK5CYII='
+current_frame_encoded_as_response = [None]
+current_frame_encoded_as_response[0] = Response(content=base64.b64decode(black_1px.encode('ascii')), media_type='image/png')
+
+def convert(frame: np.ndarray) -> bytes:
+    _, imencode_image = cv2.imencode('.jpg', frame)
+    return imencode_image.tobytes()
+
+@event_emitter.on('annotated_frame')
+def lm_ann_frame(frame):
+    # Encode the image to a memory buffer
+    jpeg = convert(frame)
+    current_frame_encoded_as_response[0] = Response(content=jpeg, media_type='image/jpeg')
+
+@app.get('/video/frame')
+async def grab_video_frame() -> Response:
+    return current_frame_encoded_as_response[0]
+
+@ui.page('/')
+def show():
+    ui.markdown('# Pose Trigger')
+
+    with ui.card().tight():
+        video_image = ui.interactive_image().classes('w-full h-full')
+        ui.timer(interval=0.2, callback=lambda: video_image.set_source(f'/video/frame?{time.time()}'))
+        with ui.card_section():
+            ui.label('Lorem ipsum dolor sit amet, consectetur adipiscing elit, ...')
+
+ui.run_with(app)
--- landmarker.py
+++ landmarker.py
@@ -2,10 +2,13 @@ import logging
 import threading
 import time
 import os
-from dotenv import load_dotenv
+import math
 
 import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
 import cv2
+import numpy as np
 
 BaseOptions = mp.tasks.BaseOptions
 PoseLandmarker = mp.tasks.vision.PoseLandmarker
@@ -13,66 +16,135 @@ PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
 PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+format = "%(asctime)s: %(message)s"
+logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
 def current_milli_time():
     return round(time.time() * 1000)
 
-def landmarker_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-    # print('pose landmarker result: {}'.format(result))
-    # print("Got result.")
-    # print(result.pose_landmarks[0][16])
+# Borrowed from https://github.com/geaxgx/openvino_blazepose/blob/main/BlazeposeOpenvino.py#L419
+# with thanks!
+def angle_with_y(v):
+    # v: 2d vector (x,y)
+    # Returns angle in degree ofv with y-axis of image plane
+    if v[1] == 0:
+        return 90
+    angle = math.atan2(v[0], v[1])
+    return math.degrees(angle)
+
+# Borrowed from https://github.com/geaxgx/openvino_blazepose/blob/main/BlazeposeOpenvino.py#L60
+# with thanks!
+semaphore_flag = {
+    (3,4):'A', (2,4):'B', (1,4):'C', (0,4):'D',
+    (4,7):'E', (4,6):'F', (4,5):'G', (2,3):'H',
+    (0,3):'I', (0,6):'J', (3,0):'K', (3,7):'L',
+    (3,6):'M', (3,5):'N', (2,1):'O', (2,0):'P',
+    (2,7):'Q', (2,6):'R', (2,5):'S', (1,0):'T',
+    (1,7):'U', (0,5):'V', (7,6):'W', (7,5):'X',
+    (1,6):'Y', (5,6):'Z'
+}
+
+def extract_angles_from_poses(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int, event_emitter):
     age = current_milli_time() - timestamp_ms
-    print("#####")
-    print("Age", age)
+    emit_data = {}
+    emit_data['persons'] = len(result.pose_landmarks)
+    emit_data['time_to_infer'] = age
+
     if len(result.pose_landmarks) == 1:
         vone = result.pose_landmarks[0]
         if len(vone) >= 16:
-            # print("Right", vone[16].y, vone[12].y, angle_between_points(lmtp(vone[16]), lmtp(vone[12])))
-            # print("Left", vone[15].y, vone[11].y, angle_between_points(lmtp(vone[15]), lmtp(vone[11])))
-
             # Ref: https://github.com/geaxgx/openvino_blazepose/blob/main/BlazeposeOpenvino.py#L61
             # Also https://github.com/geaxgx/openvino_blazepose/blob/main/BlazeposeOpenvino.py#L417
-
             right_arm_angle = angle_with_y((vone[14].x - vone[12].x, vone[14].y - vone[12].y))
             left_arm_angle = angle_with_y((vone[13].x - vone[11].x, vone[13].y - vone[11].y))
             right_pose = int((right_arm_angle +202.5) / 45) % 8
             left_pose = int((left_arm_angle +202.5) / 45) % 8
             letter = semaphore_flag.get((right_pose, left_pose), None)
-            print("Right, left, pose, pose, letter", right_arm_angle, left_arm_angle, right_pose, left_pose, letter)
 
+            emit_data['right_arm_angle'] = right_arm_angle
+            emit_data['left_arm_angle'] = left_arm_angle
+            emit_data['right_pose'] = right_pose
+            emit_data['left_pose'] = left_pose
+            emit_data['letter'] = letter
 
-            # print("Left", vone[15].y, vone[11].y)
+    event_emitter.emit('landmarker_result', emit_data)
 
-def thread_run_mediapipe(camera_url, modelfile):
+# Ref: https://github.com/google-ai-edge/mediapipe-samples/blob/main/examples/pose_landmarker/python/%5BMediaPipe_Python_Tasks%5D_Pose_Landmarker.ipynb
+def draw_landmarks_on_image(rgb_image, detection_result):
+    pose_landmarks_list = detection_result.pose_landmarks
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected poses to visualize.
+    for idx in range(len(pose_landmarks_list)):
+        pose_landmarks = pose_landmarks_list[idx]
+
+        # Draw the pose landmarks.
+        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        pose_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+        ])
+        solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            pose_landmarks_proto,
+            solutions.pose.POSE_CONNECTIONS,
+            solutions.drawing_styles.get_default_pose_landmarks_style())
+
+    return annotated_image
+
+def thread_run_mediapipe(event_emitter, camera_url, modelfile):
     options = PoseLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=modelfile),
         running_mode=VisionRunningMode.VIDEO)
 
     with PoseLandmarker.create_from_options(options) as landmarker:
         # The landmarker is initialized. Use it here.
-        print("Opening capture...")
+        logging.info("Opening capture...")
         framecount = 0
         vcap = cv2.VideoCapture(camera_url)
+        logging.info("Capture has been opened.")
+        keep_capturing = True
 
-        while True:
-            # print("Capture...")
+        @event_emitter.on('kill_lm_thread')
+        def kill_lm_thread():
+            # Whee! Thread safety non existent!
+            keep_capturing = False
+
+        while keep_capturing:
             ret, frame = vcap.read()
             if not ret:
-                print("Error - failed to capture. Aborting.")
+                logging.error("Failed to capture - aborting.")
                 break
 
             if framecount % 5 == 0:
                 mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
                 frame_timestamp_ms = current_milli_time()
                 pose_landmarker_result = landmarker.detect_for_video(mp_image, frame_timestamp_ms)
-                landmarker_result(pose_landmarker_result, mp_image, frame_timestamp_ms)
+                extract_angles_from_poses(pose_landmarker_result, mp_image, frame_timestamp_ms, event_emitter)
+                annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), pose_landmarker_result)
+
+                event_emitter.emit('raw_frame', mp_image)
+                event_emitter.emit('annotated_frame', annotated_image)
 
             framecount += 1
 
+        vcap.release()
+
 if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    from dotenv import load_dotenv
+    from pyee.base import EventEmitter
+
     load_dotenv()
 
-    x = threading.Thread(target=thread_run_mediapipe, args=(os.getenv('CAMERA'), os.getenv('MODELFILE')))
+    ee = EventEmitter()
+
+    @ee.on('landmarker_result')
+    def lm_result_simple(foo):
+        print(foo)
+
+    @ee.on('annotated_frame')
+    def lm_ann_frame(frame):
+        cv2.imwrite('out.jpg', frame)
+
+    x = threading.Thread(target=thread_run_mediapipe, args=(ee, os.getenv('CAMERA'), os.getenv('MODELFILE')))
     logging.info("Starting capture thread...")
     x.start()
